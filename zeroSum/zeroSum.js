08d82f00de938b3b4fd4faf5a03b7a58dfb5ee21
Abdlrrahman Shibani <56532938+Abdlrrahman@users.noreply.github.com>
2019-12-31 09:23:07 +0100

    29

---
+++ zeroSum/zeroSum.js
@@ -0,0 +1,29 @@
+/*
+Zero Sum
+Given an array of negative/positive integers, return true if there exists two numbers whose sum is zero.
+
+Constraints: Solve this linear time complexity O(n)
+
+zeroSum([ 1, 3, 2, -3 ]) // true
+zeroSum([ 5, 7, 2, 9 ]) // false
+*/
+
+// I used a varible for loop and two if conditions, the first if to check for there are two numbers whose sum is zero, and the seconf if to loop recursively and slice the array in oreder to keep the time complicsity at O(n).
+
+function zeroSum(numbers) {
+	var check = numbers[0];
+
+	for (var i = 1; i < numbers.length; i++) {
+		if (check + numbers[i] === 0) {
+			return true
+		}
+		else if (i === numbers.length - 1) {
+			return zeroSum(numbers.slice(1))
+		}
+	}
+		return false;
+}
+
+
+zeroSum([ 1, 3, 2, -3 ]) // true
+zeroSum([ 5, 7, 2, 9 ]) // false
